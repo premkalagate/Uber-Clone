fa9c8def90c2cc5d3f4ad6a37041f4d521fa0379
premkalagate <premkalagate52@gmail.com>
2025-08-11 21:01:24 +0530

    captain authentication done

--- Backend/models/captain.model.js
+++ Backend/models/captain.model.js
@@ -18,7 +18,7 @@ const captainSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-        // lowercase: true,
+        lowercase: true,
         match: [ /^\S+@\S+\.\S+$/, 'Please enter a valid email'],
     },
     password:{
